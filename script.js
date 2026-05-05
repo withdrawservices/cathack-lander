9df4ebd729ed250316e96c97e2111231a31236d7
withdrawservices <withdraw.cc@gmail.com>
2026-05-04 20:58:30 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // hey papis
+
+document.body.classList.add("enter-locked");
+
+const enterScreen = document.getElementById("enterScreen");
+const enterButton = document.getElementById("enterButton");
+const enterSound = document.getElementById("enterSound");
+
+enterButton.addEventListener("click", async () => {
+  try {
+    enterSound.currentTime = 0;
+    await enterSound.play();
+  } catch (err) {
+    console.log("Sound could not play:", err);
+  }
+
+  enterScreen.classList.add("hidden");
+  document.body.classList.remove("enter-locked");
+
+  setTimeout(() => {
+    enterScreen.remove();
+  }, 650);
+});
